8b95f2ebd945a8636ebeed3fe086ab0e7737d5fb
github username <jasonbeneducci@yahoo.com>
2019-12-11 13:20:17 -0500

    building out the home page

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, Image} from 'react-native';
+import { Platform, StyleSheet, Text, View, Image, Button} from 'react-native';
 
 // const description = Platform.select({
 //   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -13,6 +13,11 @@ export default class App extends Component {
         <Text style={styles.welcome}>Paydirt Picks</Text>
         <Image style={styles.mainImage} source={require('./images/main-logo.png')}/>
         <Text style={styles.description}>NFL Pickem Application</Text>
+        <View style={styles.buttonContainer}>
+          <Button style={styles.button} onPress={() => alert("You tapped the first button!")} title="Press Me!"></Button>
+          <Button style={styles.button} onPress={() => alert("You tapped the second button!")} title="Press Me!"></Button>
+          <Button style={styles.button} onPress={() => alert("You tapped the third button!")} title="Press Me!"></Button>
+        </View>
         {/* <Text style={styles.description}>{description}</Text> */}
       </View>
     );
@@ -32,13 +37,25 @@ const styles = StyleSheet.create({
     margin: 4,
     color: 'red'
   },
-  description: {
-    textAlign: 'center',
-    color: 'white',
-    marginBottom: 5,
-  },
+  // description: {
+  //   textAlign: 'center',
+  //   color: 'white',
+  //   marginBottom: 5,
+  // },
   mainImage: {
     backgroundColor: 'white',
     borderRadius: 10
+  },
+  buttonContainer: {
+    width: '100%',
+    marginBottom: 0,
+    alignItems: 'stretch',
+    backgroundColor: 'white',
+  },
+  button: {
+    height: 20,
+    width: 20
   }
 });
+
+
---
+++ views/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {StyleSheet, Text, View, Image } from 'react-native'
+
+const Home = () => {
+    return (
+        <View style={styles.container}>
+            <Text></Text>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'red'
+    },
+    homeText: {
+        color: 'red',
+        fontSize: 60,
+        textAlign: 'center'
+    }
+})
+export default Home
