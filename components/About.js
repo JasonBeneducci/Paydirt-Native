9c586bdd13d1425fbefd5f9d8ee0b5f103b01390
github username <jasonbeneducci@yahoo.com>
2019-12-18 11:38:02 -0500

    back to working version

--- components/About.js
+++ components/About.js
@@ -1,19 +1,15 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-import { Button } from 'native-base'
+import React from 'react'
+import { TouchableOpacity, Text } from 'react-native'
+import { Actions } from 'react-native-router-flux'
 
-export default class About extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Image style={styles.mainImage} source={require('../images/main-logo.png')} />
-                <View style={styles.buttonContainer}>
-                    <Text>
-                        About Page
-                    </Text>
-                    <Button>Go to Home Page</Button>
-                </View>
-            </View>
-        );
+const About = () => {
+    const goToHome = () => {
+        Actions.home()
     }
+    return (
+        <TouchableOpacity style={{ margin: 128 }} onPress={goToHome}>
+            <Text>This is ABOUT</Text>
+        </TouchableOpacity>
+    )
 }
+export default About
--- components/Home.js
+++ components/Home.js
@@ -1,19 +1,68 @@
 import React, { Component } from 'react';
-import { Text, View, Image } from 'react-native';
-import { Button } from 'native-base'
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TouchableOpacity,
+} from 'react-native';
 
 export default class App extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Image style={styles.mainImage} source={require('../images/main-logo.png')} />
+                <Image style={styles.mainImage} source={require('./images/main-logo.png')} />
                 <View style={styles.buttonContainer}>
-                    <Text>
-                        Home Page
-                    </Text>
-                    <Button>Go to About Page</Button>
+                    <TouchableOpacity style={styles.navItem} onPress={() => alert("You should be going to make picks.")}><Text style={styles.navItemText}>Make Picks</Text></TouchableOpacity>
+                    <TouchableOpacity style={styles.navItem} onPress={() => alert("You should be going to the leaderboards.")}><Text style={styles.navItemText}>Leaderboards</Text></TouchableOpacity>
+                    <TouchableOpacity style={styles.navItem} onPress={() => alert("You should be going to the about section")}><Text style={styles.navItemText}>About</Text></TouchableOpacity>
                 </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    description: {
+        fontFamily: 'Cochin',
+        textAlign: 'center',
+        color: 'black',
+        marginBottom: 5,
+    },
+    mainImage: {
+        position: 'absolute',
+        top: 100,
+        height: 290,
+        width: 320,
+        backgroundColor: 'white',
+    },
+    buttonContainer: {
+        position: 'absolute',
+        bottom: 50,
+        display: 'flex',
+        flexDirection: 'column',
+        color: 'white',
+        width: '100%',
+    },
+    navItem: {
+        margin: 7,
+        borderStyle: 'solid',
+        backgroundColor: 'rgb(23,57,102)',
+        borderWidth: 2,
+        borderRadius: 5,
+    },
+    navItemText: {
+        margin: 7,
+        fontFamily: "Copperplate",
+        fontWeight: 'bold',
+        fontSize: 40,
+        color: 'white',
+        textAlign: 'center',
+        backgroundColor: 'rgb(178,37,29)',
+    }
+});
